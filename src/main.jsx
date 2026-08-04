149d15b4b83e8099ed6afea5703579071563b536
Benhur Senabathi <67542202+benhursenabathi@users.noreply.github.com>
2026-08-04 22:59:45 +0100

    Unify secondary pages and blog purchase CTAs

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
+import { StrictMode, useLayoutEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function AppRoot() {
+  useLayoutEffect(() => {
+    document.documentElement.classList.add('app-ready')
+  }, [])
+
+  return <App />
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppRoot />
   </StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,43 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+const STATIC_PAGE_ROUTES = new Set([
+  '/switchy/compare/',
+  '/switchy/blog/',
+  '/switchy/blog/how-to-switch-magic-keyboard-between-macs/',
+  '/switchy/blog/magic-keyboard-pairing-mode/',
+  '/switchy/blog/magic-keyboard-multiple-devices/',
+  '/switchy/blog/universal-control-vs-switching-devices/',
+  '/switchy/blog/kvm-switch-for-two-macs/',
+  '/switchy/blog/one-keyboard-mouse-mac-mini-macbook/',
+  '/switchy/privacy/',
+])
+
+function staticPageRoutes() {
+  return {
+    name: 'switchy-static-page-routes',
+    enforce: 'pre',
+    configureServer(server) {
+      server.middlewares.use((request, _response, next) => {
+        const requestUrl = request.url || ''
+        const queryStart = requestUrl.indexOf('?')
+        const pathname = queryStart === -1 ? requestUrl : requestUrl.slice(0, queryStart)
+
+        const normalizedPathname = pathname.endsWith('/') ? pathname : `${pathname}/`
+
+        if (STATIC_PAGE_ROUTES.has(normalizedPathname)) {
+          const query = queryStart === -1 ? '' : requestUrl.slice(queryStart)
+          request.url = `${normalizedPathname}index.html${query}`
+        }
+
+        next()
+      })
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [staticPageRoutes(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
